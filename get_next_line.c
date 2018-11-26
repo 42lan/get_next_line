a2a8312069c79e591d839412b6e361a492fbbb60
4slan <amalsago@student.42.fr>
2018-11-26 19:44:42 +0100

    new version

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,21 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 09:12:15 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/26 16:21:43 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/26 19:42:34 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-static void		cut_until_new_line(char *tmp)
-{
-	char	*ptr;
-	ptr = ft_strchr(tmp, '\n');
-	if (ptr)
-		*ptr = '\0';
-}
-
 int				get_next_line(const int fd, char **line)
 {
 	int				bytes_read;
 	char			buff[BUFF_SIZE + 1];
 	static char		*tmp;
+	char			*ptr;
 
-	if (fd < 0 || !(line))
+	if (fd < 0 || !line)
 		return (-1);
 	tmp = (char *)ft_memalloc(sizeof(char));
 	while ((bytes_read = read(fd, buff, BUFF_SIZE)) > 0)
@@ -37,7 +30,10 @@ int				get_next_line(const int fd, char **line)
 		if (ft_strchr(buff, '\n') != NULL)
 			break ;
 	}
-	cut_until_new_line(tmp);
-	*line = ft_strdup(tmp);
+	if (bytes_read <= 0 && !ft_strlen(tmp))
+		return (bytes_read);
+	
+	ptr = ft_strchr(tmp, '\0');
+	*line = ft_strsub(tmp, tmp + ptr, ); //strSUB
 	return (1);
 }
