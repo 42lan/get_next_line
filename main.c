95e6077a85474c3de7f030835002764d846e8574
4slan <amalsago@student.42.fr>
2018-11-27 20:47:35 +0100

    add of sleep fucntion

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 11:04:28 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/26 16:10:31 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/27 20:05:53 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <stdio.h>
 #include "get_next_line.h"
 #include "libft/libft.h"
 
@@ -26,26 +27,11 @@ int		main(int argc, char **argv)
 	}
 
 	fd = open(argv[1], O_RDONLY);
-	if (get_next_line(fd, &line) == 1)
+	while (get_next_line(fd, &line) == 1)
 	{
-		ft_putstr("\n-----------FIRST CALL----------------\n");
-		ft_putchar('|');
-		ft_putstr(line);
-		ft_putchar('|');
-	}
-	if (get_next_line(fd, &line) == 1)
-	{
-		ft_putstr("\n----------SECOND CALL--------------\n");
-		ft_putchar('|');
-		ft_putstr(line);
-		ft_putchar('|');
-	}
-	if (get_next_line(fd, &line) == 1)
-	{
-		ft_putstr("\n----------THIRD CALL--------------\n");
-		ft_putchar('|');
-		ft_putstr(line);
-		ft_putchar('|');
+		ft_putendl(line);
+		sleep(1);
+		free(line);
 	}
 	close(fd);
 	return (0);
