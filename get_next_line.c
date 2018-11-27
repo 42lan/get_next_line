18de19efd1230f1e22c3b4604c270dc523c66b86
4slan <amalsago@student.42.fr>
2018-11-27 20:44:41 +0100

    Added: new function to found posiition of newline character; condition to verify if i'm at the end of string; if stattement to check if tmp ism't empty

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,35 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 09:12:15 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/26 19:42:34 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/27 18:57:56 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
+#include <string.h>
+
+int				ft_strclen(const char *s, int c)
+{
+	int		index;
+
+	index = 0;
+	while (s[index] != '\0' && s[index] != c)
+		index++;
+	return (index);
+}
 
 int				get_next_line(const int fd, char **line)
 {
 	int				bytes_read;
 	char			buff[BUFF_SIZE + 1];
 	static char		*tmp;
-	char			*ptr;
+	size_t			nl;
 
 	if (fd < 0 || !line)
 		return (-1);
-	tmp = (char *)ft_memalloc(sizeof(char));
+	if (!tmp)
+		tmp = (char *)ft_memalloc(sizeof(char));
 	while ((bytes_read = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[bytes_read] = '\0';
@@ -33,7 +45,11 @@ int				get_next_line(const int fd, char **line)
 	if (bytes_read <= 0 && !ft_strlen(tmp))
 		return (bytes_read);
 	
-	ptr = ft_strchr(tmp, '\0');
-	*line = ft_strsub(tmp, tmp + ptr, ); //strSUB
+	nl = ft_strclen(tmp, '\n');
+	*line = ft_strsub(tmp, 0, nl);
+	if (tmp[nl] != '\0')
+		tmp = ft_strcpy(tmp, (tmp + nl) + 1);
+	else
+		ft_strdel(&tmp);
 	return (1);
 }
