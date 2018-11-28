28ffbf06b052ac11e50d92913e5560870ed43573
4slan <amalsago@student.42.fr>
2018-11-28 09:43:10 +0100

    buff size changed

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 09:24:03 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/26 16:16:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/28 09:34:05 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 1000
 
 int		get_next_line(const int fd, char **line);
 
