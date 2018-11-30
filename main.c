7ca44ff894d287dd7ebb803f7d10dedcfbcfdc1d
4slan <amalsago@student.42.fr>
2018-11-30 11:59:50 +0100

    Norm correction : functions to open and close fd added

--- main.c
+++ main.c
@@ -6,41 +6,49 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 11:04:28 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/29 17:41:14 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/11/30 11:51:47 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
 #include "get_next_line.h"
 #include "libft/libft.h"
-#include <time.h> ////////////////////////////////// SUPPRIMER //////////////////////////////////////
+#include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
+
+void	open_fd(int *fd1, int *fd2, char **argv)
+{
+	*fd1 = open(argv[1], O_RDONLY);
+	*fd2 = open(argv[2], O_RDONLY);
+}
+
+void	close_fd(int *fd1, int *fd2)
+{
+	close(*fd1);
+	close(*fd2);
+}
 
 int		main(int argc, char **argv)
 {
+	char		*line;
+	int			fd1;
+	int			fd2;
+	clock_t		begin;
+	clock_t		end;
+
 	(void)argc;
-	char	*line;
-	int		fd1 = open(argv[1], O_RDONLY);
-	int		fd2 = open(argv[2], O_RDONLY);
-	clock_t begin = clock();
-	while (get_next_line(fd1, &line) == 1)
+	open_fd(&fd1, &fd2, argv);
+	begin = clock();
+	while (get_next_line(fd1, &line))
 	{
-		ft_putstr("fd1: \t"); ft_putendl(line);		// print 1st line of 1st fd (fd1)
+		printf("fd1: \t%s\n", line);
 		if (fd2 > 0)
-			if (get_next_line(fd2, &line) > 0)
-			{
-				ft_putstr("fd2: \t");
-				ft_putendl(line);		// print 1st line of 2nd fd (fd2)
-			}
-		//usleep(50000);
+			if (get_next_line(fd2, &line))
+				printf("fd2: \t%s\n", line);
 		free(line);
 	}
-	clock_t end = clock();
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("%f\n\n", time_spent);
-	ft_putnbr(OPEN_MAX);
-	ft_putendl(" = OPEN_MAX");
-	close(fd1);
-	close(fd2);
+	end = clock();
+	printf("%f\n\n", (double)(end - begin) / CLOCKS_PER_SEC);
+	close_fd(&fd1, &fd2);
 	return (0);
 }
