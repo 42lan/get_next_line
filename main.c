335787821dc66b5f371c3aa348f1173962061e94
Aslan MALSAGOV <aslan.malsag@gmail.com>
2018-12-02 13:33:36 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,49 +6,24 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 11:04:28 by amalsago          #+#    #+#             */
-/*   Updated: 2018/11/30 11:51:47 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/01 20:47:42 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft/libft.h"
+#include "libft/includes/libft.h"
 #include <fcntl.h>
 #include <stdio.h>
-#include <time.h>
-
-void	open_fd(int *fd1, int *fd2, char **argv)
-{
-	*fd1 = open(argv[1], O_RDONLY);
-	*fd2 = open(argv[2], O_RDONLY);
-}
-
-void	close_fd(int *fd1, int *fd2)
-{
-	close(*fd1);
-	close(*fd2);
-}
 
 int		main(int argc, char **argv)
 {
 	char		*line;
 	int			fd1;
-	int			fd2;
-	clock_t		begin;
-	clock_t		end;
 
 	(void)argc;
-	open_fd(&fd1, &fd2, argv);
-	begin = clock();
-	while (get_next_line(fd1, &line))
-	{
-		printf("fd1: \t%s\n", line);
-		if (fd2 > 0)
-			if (get_next_line(fd2, &line))
-				printf("fd2: \t%s\n", line);
-		free(line);
-	}
-	end = clock();
-	printf("%f\n\n", (double)(end - begin) / CLOCKS_PER_SEC);
-	close_fd(&fd1, &fd2);
+	fd1 = open(argv[1], O_RDONLY);
+	while (get_next_line(1000000, &line) == 1)
+		ft_putendl(line);
+	close(fd1);
 	return (0);
 }
